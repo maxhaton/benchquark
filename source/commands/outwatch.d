2f1d3562e626c8cfc235ff70a3d81e0771a33656
Max Haughton <maxhaton@gmail.com>
2019-10-10 23:11:37 +0100

    Outwatch working

--- source/commands/outwatch.d
+++ source/commands/outwatch.d
@@ -3,28 +3,35 @@ module commands.outwatch;
 import util.config;
 import std.process;
 import std.datetime;
-void command(inout BuildSpecification build)
+import std.format;
+import util.inform;
+int command(inout BuildSpecification build)
 {
     import std.stdio;
     auto timeMut = Clock.currTime;
-    
-    auto pipe = pipeShell(build.shellString, Redirect.stdout | Redirect.stderr);
+    inform(format!"Outwatch executing -> %s"(build.exactCommand));
+    auto pipe = pipeShell(build.exactCommand, Redirect.stdout | Redirect.stderr);
 
     auto output = pipe.stdout;
     
-    while(!output.eof)
+    while(!pipe.pid.tryWait.terminated)
     {
+        import std.range : padRight, array;
         const startTime = Clock.currTime;
 
         
-        const line = output.readln;
+        const line = output.readln;  
 
-        writef!"%s | %s"(startTime - timeMut, line);
+        //N.B. line isn't stripped so \n is not required
+        const dur = startTime - timeMut;
+        const leftSide = format!"%d ms"(dur.total!"msecs").padRight(' ', 8).array;
+        writef!"%s| %s"(leftSide, line);
 
         timeMut = startTime;
     }
     //Wait to finish
     pipe.pid.wait;
+    return 0;
 }
 
 
